2837ce05a998a0b8b4ad13eb89cb33c70481c458
tskdvaraprasad <varaprasad95911@gmail.com>
2026-07-27 11:51:13 +0530

    initial commit

---
+++ Identifiers_1.py
@@ -0,0 +1,22 @@
+# Variable
+student = "Vara"
+
+# Constant-style name (written in uppercase by convention)
+PI = 3.14159
+
+# Function name
+def greet():
+    return "Hello!"
+
+# Class name
+class Student:
+    pass
+
+# Name using an underscore
+student_age = 18
+# Print all of them
+print("Variable:", student)
+print("Constant-style name:", PI)
+print("Function name:", greet())
+print("Class name:", Student.__name__)
+print("Name using underscore:", student_age)
