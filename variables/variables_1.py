7533d8c3a18704d13b4aa95caf9a6212904bdaff
tskdvaraprasad <varaprasad95911@gmail.com>
2026-07-27 22:51:53 +0530

    initial commits

---
+++ variables/variables_1.py
@@ -0,0 +1,27 @@
+# Declare variables
+name = "Vara Prasad"
+age = 19
+height = 5.11
+is_student = True
+
+# Print values and their types
+print("Name:", name)
+print("Type:", type(name))
+
+print("Age:", age)
+print("Type:", type(age))
+
+print("Height:", height)
+print("Type:", type(height))
+
+print("Student:", is_student)
+print("Type:", type(is_student))
+
+#Name: Vara Prasad
+#Type: <class 'str'>
+#ge: 19
+#Type: <class 'int'>
+#Height: 5.11
+#Type: <class 'float'>
+#Student: True
+#Type: <class 'bool'>
